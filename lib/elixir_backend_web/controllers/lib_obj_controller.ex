572f2461c4283fefbc50c4b6589d1b593573a465
The Major <spartan364@gmail.com>
2021-12-12 16:29:09 +0000

    make error return 400 instead

--- lib/elixir_backend_web/controllers/lib_obj_controller.ex
+++ lib/elixir_backend_web/controllers/lib_obj_controller.ex
@@ -7,27 +7,41 @@ defmodule ElixirBackendWeb.LibObjController do
   plug :verify_params
 
   def fetch(conn, %{"obj" => kind} = params) do
-    #chips = ElixirBackend.Repo.all(Battlechip)
-    #json(conn, chips)
+    # chips = ElixirBackend.Repo.all(Battlechip)
+    # json(conn, chips)
 
-    conds = conn.assigns.kind.gen_conditions(params)
-    query = from conn.assigns.kind, where: ^conds
+    objs =
+      try do
+        conds = conn.assigns.kind.gen_conditions(params)
+        query = from(conn.assigns.kind, where: ^conds)
 
-    objs = ElixirBackend.Repo.all(query)
-    #json(conn, objs)
+        {:ok, ElixirBackend.Repo.all(query)}
+      rescue
+        e ->
+          {:error, e}
+      end
 
-    render(conn, "libobj.json", kind: kind, objs: objs)
+    case objs do
+    {:ok, objs} ->
+      render(conn, "libobj.json", kind: kind, objs: objs)
+    {:error, _e} ->
+      send_resp(conn, 400, "bad parameter")
+    end
 
+    # json(conn, objs)
   end
 
   defp verify_params(conn, _) do
     case conn.params["obj"] do
       "ncps" ->
         conn |> assign(:kind, NCP)
+
       "viruses" ->
         conn |> assign(:kind, Virus)
+
       "chips" ->
         conn |> assign(:kind, Battlechip)
+
       _ ->
         conn |> send_resp(404, "Only chips, ncps, and viruses are allowed") |> halt()
     end
