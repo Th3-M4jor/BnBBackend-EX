9a35d39981cadf3b4361a15ce8d0002561666d22
The Major <spartan364@gmail.com>
2021-12-17 23:39:39 +0000

    update to use new built-in Map.filter fn

--- lib/elixir_backend_web/views/lib_obj_view.ex
+++ lib/elixir_backend_web/views/lib_obj_view.ex
@@ -8,43 +8,43 @@ defmodule ElixirBackendWeb.LibObjView do
 
   def render("libobj.json", %{kind: "chips", objs: chips}) do
     Enum.map(chips, fn chip ->
-      :maps.filter(&chips_filter_fn/2, chip)
+      Map.filter(chip, &chips_filter_fn/1)
     end)
   end
 
   def render("libobj.json", %{kind: "viruses", objs: viruses}) do
     Enum.map(viruses, fn virus ->
-      :maps.filter(&virus_filter_fn/2, virus)
+      Map.filter(virus, &virus_filter_fn/1)
     end)
   end
 
   def render("libobj.json", %{kind: "ncps", objs: ncp}) do
     Enum.map(ncp, fn ncp ->
-      :maps.filter(&ncp_filter_fn/2, ncp)
+      Map.filter(ncp, &ncp_filter_fn/1)
     end)
   end
 
-  defp chips_filter_fn(key, value) when key in @chip_props and not is_nil(value) do
+  defp chips_filter_fn({key, value}) when key in @chip_props and not is_nil(value) do
     true
   end
 
-  defp chips_filter_fn(_, _) do
+  defp chips_filter_fn({_, _}) do
     false
   end
 
-  defp virus_filter_fn(key, value) when key in @virus_props and not is_nil(value) do
+  defp virus_filter_fn({key, value}) when key in @virus_props and not is_nil(value) do
     true
   end
 
-  defp virus_filter_fn(_, _) do
+  defp virus_filter_fn({_, _}) do
     false
   end
 
-  defp ncp_filter_fn(key, value) when key in @ncp_props and not is_nil(value) do
+  defp ncp_filter_fn({key, value}) when key in @ncp_props and not is_nil(value) do
     true
   end
 
-  defp ncp_filter_fn(_, _) do
+  defp ncp_filter_fn({_, _}) do
     false
   end
 
